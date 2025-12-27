8cb72dd2442588c09088e720c5743a47c901cd59
causon-mikolorenz <causonmikolorenz@gmail.com>
2025-12-27 19:35:38 +0800

    add create user stored procedure

--- database/migrations/procedures.go
+++ database/migrations/procedures.go
@@ -38,4 +38,57 @@ var Procedures = []Migration{
 			COMMIT;
 		END;`,
 	},
+	{
+		Name: "Create User Procedure",
+		SQL: `
+		-- Drop the procedure first
+		DROP PROCEDURE IF EXISTS CreateUser;
+		-- Then create to become idempotent
+		CREATE PROCEDURE CreateUser(
+			IN userId BINARY(16),
+			IN username varchar(255),
+			IN firstName VARCHAR(50),
+			IN middleName VARCHAR(50),
+			IN lastName VARCHAR(50),
+			IN userEmail VARCHAR(100),
+			IN userPasswordHash VARCHAR(255)
+		)
+		BEGIN
+			DECLARE EXIT HANDLER FOR SQLEXCEPTION
+			BEGIN
+				ROLLBACK;
+			END;
+
+			START TRANSACTION;
+
+			-- Archive user by setting deleted_at
+			INSERT INTO users (
+				id,
+				username,
+				first_name,
+				middle_name,
+				last_name,
+				email,
+				password_hash
+			)
+			VALUES (
+				userId,
+				username,
+				firstName,
+				middleName,
+				lastName,
+				userEmail,
+				userPasswordHash
+			);
+			
+			-- Ilagay sa audit logs
+			INSERT INTO audit_logs (user_id, action, details)
+			VALUES (
+				userId, 
+				'Create user', 
+				CONCAT('User ', HEX(userId), ' was created.')
+			);
+			COMMIT;
+		END;`,
+	},
 }
