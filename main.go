2f6b923915065ce3a0aa15f6d3221c7238f4bde0
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-01-02 22:29:18 +0800

    implement idempotent migration runner

--- main.go
+++ main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
+	"github.com/causon-mikolorenz/unified-access-backend/database"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
+
+	// Create flag for migration
+	doMigrate := flag.Bool("migrate", false, "Run database migration first")
+	flag.Parse()
+
+	if *doMigrate {
+		// Connect to the database with admin creds
+		adminDatabase, err := database.ConnectAdminToDB()
+		// Check for errors
+		if err != nil {
+			log.Fatalf(
+				"Error starting database with admin credentials: %v",
+				err,
+			)
+		}
+		// Close before exiting
+		defer adminDatabase.Close()
+
+		// Do the migrations
+		err = database.RunAllMigrations(adminDatabase)
+		fmt.Println("Database Migrated Successfully")
+
+		// Exit after migrating
+		return
+	}
+
+	// Connect to the database
+	appDB, err := database.ConnectToDB()
+	if err != nil {
+		log.Fatalf("Application Database failed to start: %v", err)
+	}
+
+	// Close the Application DB when the program exits
+	defer appDB.Close()
+
+	fmt.Println("Backend is running!")
+
+	r := gin.Default()
+	r.Run(":8080")
 }
