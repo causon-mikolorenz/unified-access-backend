ed0629f8f6b38fb614247a335bec3e45f21a889b
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-01-05 14:37:41 +0800

    define domain model for user

---
+++ models/user.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           []byte    `json:"id" db:"id"`
+	Username     string    `json:"userName" db:"username"`
+	FirstName    string    `json:"firstName" db:"first_name"`
+	MiddleName   string    `json:"middleName" db:"middle_name"`
+	LastName     string    `json:"lastName" db:"last_name"`
+	Email        string    `json:"email" db:"email"`
+	PasswordHash string    `json:"-" db:"password_hash"`
+	Status       string    `json:"status" db:"status"`
+	UpdatedAt    time.Time `json:"updatedAt" db:"updated_at"`
+}
