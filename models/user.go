cf664986274521028a31a1cfacf0fa5ced62dcf8
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-01-05 22:30:06 +0800

    add helpers for userstatus

--- models/user.go
+++ models/user.go
@@ -11,6 +11,18 @@ const (
 	StatusDeleted   UserStatus = "deleted"
 )
 
+func (s UserStatus) CanLogin() bool {
+	return s == StatusActive
+}
+
+func (s UserStatus) RequiresAction() bool {
+	return s == StatusInactive
+}
+
+func (s UserStatus) IsRestricted() bool {
+	return s == StatusSuspended || s == StatusDeleted
+}
+
 type User struct {
 	ID           []byte     `json:"id" db:"id"`
 	Username     string     `json:"userName" db:"username"`
