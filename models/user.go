8c54d2287e8b030326f4a0b6a9a03a7dcfe6185b
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-01-05 22:23:10 +0800

    add constants for status enum

--- models/user.go
+++ models/user.go
@@ -2,16 +2,25 @@ package models
 
 import "time"
 
+type UserStatus string
+
+const (
+	StatusActive    UserStatus = "active"
+	StatusInactive  UserStatus = "inactive"
+	StatusSuspended UserStatus = "suspended"
+	StatusDeleted   UserStatus = "deleted"
+)
+
 type User struct {
-	ID           []byte    `json:"id" db:"id"`
-	Username     string    `json:"userName" db:"username"`
-	FirstName    string    `json:"firstName" db:"first_name"`
-	MiddleName   string    `json:"middleName" db:"middle_name"`
-	LastName     string    `json:"lastName" db:"last_name"`
-	Email        string    `json:"email" db:"email"`
-	PasswordHash string    `json:"-" db:"password_hash"`
-	Status       string    `json:"status" db:"status"`
-	UpdatedAt    time.Time `json:"updatedAt" db:"updated_at"`
+	ID           []byte     `json:"id" db:"id"`
+	Username     string     `json:"userName" db:"username"`
+	FirstName    string     `json:"firstName" db:"first_name"`
+	MiddleName   string     `json:"middleName" db:"middle_name"`
+	LastName     string     `json:"lastName" db:"last_name"`
+	Email        string     `json:"email" db:"email"`
+	PasswordHash string     `json:"-" db:"password_hash"`
+	Status       UserStatus `json:"status" db:"status"`
+	UpdatedAt    time.Time  `json:"updatedAt" db:"updated_at"`
 
 	Roles []string `json:"roles" db:"-"`
 }
