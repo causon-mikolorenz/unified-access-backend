3d0ea6bad696752229f173c713e86bfe3a0d98ab
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-01-05 18:16:43 +0800

    define additional structs related to client.go

--- models/client.go
+++ models/client.go
@@ -8,3 +8,14 @@ type Client struct {
 	ClientSecret string    `json:"-" db:"client_secret"`
 	UpdatedAt    time.Time `json:"updatedAt" db:"updated_at"`
 }
+
+type ClientUrl struct {
+	ID          int    `json:"id" db:"id"`
+	ClientID    []byte `json:"clientId" db:"client_id"`
+	RedirectURL string `json:"redirectUrl" db:"redirect_url"`
+}
+
+type ClientGrantType struct {
+	ClientID  []byte `json:"clientId" db:"client_id"`
+	GrantType string `json:"grantType" db:"grant_type"`
+}
