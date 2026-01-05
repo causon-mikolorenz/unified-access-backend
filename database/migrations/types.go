5cadce7d161f3cb206bdcb122dbd72662da60279
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-01-06 02:11:03 +0800

    update type declaration for migration struct

--- database/migrations/types.go
+++ database/migrations/types.go
@@ -1,6 +1,11 @@
 package migrations
 
-type Migration struct {
-	Name string
-	SQL  string
+type MigrationStep struct {
+	ID  string // Unique ID for this specific change (e.g., "users-create", "users-add-phone")
+	SQL string
+}
+
+type TableMigration struct {
+	TableName string
+	Steps     []MigrationStep
 }
