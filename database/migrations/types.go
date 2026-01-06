ca9e376ca902a0b006b63919ed57d7f044609331
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-01-06 13:14:46 +0800

    add type definition for a migration part

--- database/migrations/types.go
+++ database/migrations/types.go
@@ -1,5 +1,10 @@
 package migrations
 
+type MigrationPart struct {
+	Name string
+	SQL  string
+}
+
 type MigrationStep struct {
 	ID  string // Unique ID for this specific change (e.g., "users-create", "users-add-phone")
 	SQL string
