19ecb616984c3f4c9fc2fd18e125064eb85792a1
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-02-12 16:31:37 +0800

    create role data access object

---
+++ internal/dto/role_dto.go
@@ -0,0 +1,21 @@
+package dto
+
+type RoleRequest struct {
+	RoleName    string `json:"role_name"`
+	Description string `json:"description"`
+}
+
+type RoleResponse struct {
+	ID          int    `json:"id"`
+	RoleName    string `json:"role_name"`
+	Description string `json:"description"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+type RoleListRequest struct {
+	Roles       []RoleResponse `json:"roles"`
+	TotalCount  int            `json:"total_count"`
+	CurrentPage int            `json:"current_page"`
+	LastPage    int            `json:"last_page"`
+}
